f749dfec90f200cf6851c7db4e8190382c76e147
brennanpayne <brennan.s.payne@gmail.com>
2013-09-07 15:56:42 -0700

    Now displaying my fixtures

--- js/models/todo.js
+++ js/models/todo.js
@@ -1,4 +1,22 @@
-Todos.todo = DS.Model.extend({
+Todos.Todo = DS.Model.extend({
   title: DS.attr('string'),
   isCompleted: DS.attr('boolean')
 })
+
+Todos.Todo.FIXTURES = [
+  {
+    id:1,
+    title: 'Practice Javascript',
+    isCompleted: false
+  },
+  {
+    id: 2,
+    title: '...',
+    isCompleted: true
+  },
+  {
+    id: 3,
+    title: 'Profit?!',
+    isCompleted: false
+  }
+];
